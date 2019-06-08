1ef6b86ed1ca480130693f2a3be064052de3657b
Lucas <biasonlucky@hotmail.com>
2019-06-08 16:42:20 -0300

    Copying de Designer Patterns Course 's files

--- python521/blueprints/auth.py
+++ python521/blueprints/auth.py
@@ -15,6 +15,20 @@ def get_sign_in():
 
     return flask.render_template('sign-in.html', context=context)
 
+
+@blueprint.route('/sign-up', methods=[ 'GET', 'POST' ])
+def get_sign_up():    
+
+    context = {
+        'page': 'sign-up',
+        'route': {
+            'is_public': True
+        },
+    }
+
+    return flask.render_template('sign-up.html', context=context)
+
+
 @blueprint.route('/sign-in', methods=[ 'POST' ])
 def post_sign_in():
     return ''
